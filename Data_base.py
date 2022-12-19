2bd1bc3ca1c8d9daf111c6bb177ec23d27e2f5fb
Alexandr <artamoshkinas@gmail.com>
2022-12-19 20:10:56 +0700

    Добвалена возможность сохранять данные в форматах json и csv

--- Data_base.py
+++ Data_base.py
@@ -1,8 +1,23 @@
 
 import json
+import csv
 
 
+def records_data_csv(first_name, last_name, phone_number):
+    with open('Directory.csv', 'a', newline='') as csvfile:
+        fieldnames = ['Имя', 'Фамилия', 'Номер телефона']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+        writer.writerow({'Имя': first_name,
+                         'Фамилия': last_name,
+                         'Номер телефона': phone_number})
 
+def record_data_json(first_name, last_name, phone_number):
+    data = {'Имя': first_name,
+            'Фамилия': last_name,
+            'Номер телефона': phone_number}
+
+    with open('Directory_json.txt', 'a') as outfile:
+        json.dump(data, outfile)
 
 
 def record_data_txt(first_name, last_name, phone_number):
